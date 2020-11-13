0c1c568486dfecdabede966a85244439859ae522
Randy <o1984531@gmail.com>
2020-11-13 17:55:41 +0800

    Adjust the access modifier of ViewUtils

--- LibraryCollections/LibraryCollections/util/ViewUtils.swift
+++ LibraryCollections/LibraryCollections/util/ViewUtils.swift
@@ -1,11 +1,11 @@
 import Foundation
 import UIKit
 
-class ViewUtils {
+public class ViewUtils {
     
     static var loadingView:CustomLoadingView? = nil
     
-    static func closeKeyboard<T>(obj:T) {
+    public static func closeKeyboard<T>(obj:T) {
         var view:UIView? = nil
         if #available(iOS 13, *), obj is UIWindowSceneDelegate {
             let rootVC = (obj as! UIWindowSceneDelegate).window??.rootViewController
@@ -23,16 +23,14 @@ class ViewUtils {
         v.endEditing(true)
     }
     
-    static func showLoading(container:UIView, imageUrl:URL, msg:String) {
+    public static func showLoading(container:UIView, imageUrl:URL, msg:String) {
         closeLoading()
         
-        if loadingView == nil {
-            loadingView = CustomLoadingView(frame: .zero, imageUrl: imageUrl, msg: msg)
-        }
+        loadingView = CustomLoadingView(frame: .zero, imageUrl: imageUrl, msg: msg)
         loadingView?.showLoading(container: container)
     }
     
-    static func closeLoading() {
+    public static func closeLoading() {
         loadingView?.closeLoading()
         loadingView = nil
     }
