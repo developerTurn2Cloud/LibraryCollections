170a01e649e284791dcb5bc898f55a507781fbe8
Randy <o1984531@gmail.com>
2020-11-16 11:07:51 +0800

    Adjust the access modifier

--- LibraryCollections/LibraryCollections/component/NibLoadable.swift
+++ LibraryCollections/LibraryCollections/component/NibLoadable.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-protocol NibLoadable where Self: UIView {
+public protocol NibLoadable where Self: UIView {
 
     /// Setup this view with nib:
     /// 1. Load content view from nib (with the class name)
@@ -11,7 +11,7 @@ protocol NibLoadable where Self: UIView {
 
 extension NibLoadable {
     @discardableResult
-    func fromNib() -> UIView? {
+    public func fromNib() -> UIView? {
         let contentView = Bundle(for: type(of: self)).loadNibNamed(String(describing: type(of: self)), owner: self, options: nil)?.first as! UIView
         self.addSubview(contentView)
         contentView.translatesAutoresizingMaskIntoConstraints = false
