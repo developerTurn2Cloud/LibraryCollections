5611a7fda7d51ec7d94a46f003839ed81ca2ceff
Randy <o1984531@gmail.com>
2020-11-16 10:17:34 +0800

    Add extensions
    1. CALayer
    2. CGFloat
    3. Date
    4. String
    5. UIViewController

---
+++ LibraryCollections/LibraryCollections/extension/UIViewControllerExtension.swift
@@ -0,0 +1,33 @@
+import Foundation
+import UIKit
+
+extension UIViewController {
+    
+    func showAlert(title:String,
+                   content:String,
+                   conformTitle:String? = NSLocalizedString("Global.Confirm", comment: ""),
+                   confirmHandler:((UIAlertAction) -> Void)? = nil,
+                   cancelTitle:String? = nil,
+                   cancelHandler:((UIAlertAction) -> Void)? = nil) {
+        let alert:UIAlertController = UIAlertController(title: title, message: content, preferredStyle: .alert)
+        
+        if cancelTitle != nil {
+            let confirmHandler:UIAlertAction = UIAlertAction(title: conformTitle, style: .default, handler: confirmHandler)
+            let cancelAction:UIAlertAction = UIAlertAction(title: cancelTitle, style: .cancel, handler: cancelHandler)
+            
+            alert.addAction(confirmHandler)
+            alert.addAction(cancelAction)
+        } else {
+            let confirmHandler:UIAlertAction = UIAlertAction(title: conformTitle, style: .cancel, handler: confirmHandler)
+            
+            alert.addAction(confirmHandler)
+        }
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    func openShare(sharedObjects:[Any]) {
+        let activityViewController = UIActivityViewController(activityItems : sharedObjects, applicationActivities: nil)
+        activityViewController.popoverPresentationController?.sourceView = self.view
+        self.present(activityViewController, animated: true, completion: nil)
+    }
+}
