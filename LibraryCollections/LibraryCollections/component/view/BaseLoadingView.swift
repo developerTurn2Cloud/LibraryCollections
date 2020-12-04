36b91a2def524de77e341a2b3fe6ade7c2f31735
Randy <o1984531@gmail.com>
2020-12-04 14:41:34 +0800

    Fix some error and adjust the access modifier

--- LibraryCollections/LibraryCollections/component/view/BaseLoadingView.swift
+++ LibraryCollections/LibraryCollections/component/view/BaseLoadingView.swift
@@ -1,17 +1,17 @@
 import Foundation
 import UIKit
 
-class BaseLoadingView: UIView {
-    init(frame: CGRect, imageUrl:URL, msg:String) {
+public class BaseLoadingView: UIView {
+    public init(frame: CGRect, imageUrl:URL, msg:String) {
         super.init(frame: frame)
-        self.initView()
+        self.initLoadingContentView()
     }
     
-    required init?(coder: NSCoder) {
+    public required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    override func initLoadingContentView() {
+    open func initLoadingContentView() {
         fatalError("init(coder:) has not been implemented")
     }
     
