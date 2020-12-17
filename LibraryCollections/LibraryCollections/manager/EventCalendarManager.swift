66525a1d2ed42a36f5c174f6d72a04022fcb3199
Randy <o1984531@gmail.com>
2020-12-17 15:46:50 +0800

    Add EventCalendarManager

---
+++ LibraryCollections/LibraryCollections/manager/EventCalendarManager.swift
@@ -0,0 +1,35 @@
+import Foundation
+import EventKit
+
+public class EventCalendarManager {
+    
+    public static let share:EventCalendarManager = EventCalendarManager()
+    
+    let eventStore : EKEventStore = EKEventStore()
+    
+    private init() {}
+    
+    func addEventToCalendar(title: String, description: String?, startDate: Date, endDate: Date, completion: ((_ success: Bool, _ error: NSError?) -> Void)? = nil) {
+        let eventStore = EKEventStore()
+        
+        eventStore.requestAccess(to: .event, completion: { (granted, error) in
+            if (granted) && (error == nil) {
+                let event = EKEvent(eventStore: eventStore)
+                event.title = title
+                event.startDate = startDate
+                event.endDate = endDate
+                event.notes = description
+                event.calendar = eventStore.defaultCalendarForNewEvents
+                do {
+                    try eventStore.save(event, span: .thisEvent)
+                } catch let e as NSError {
+                    completion?(false, e)
+                    return
+                }
+                completion?(true, nil)
+            } else {
+                completion?(false, error as NSError?)
+            }
+        })
+    }
+}
