bfa9ad9c5388b2a86e390e5270049bc6c84d1b83
Randy <o1984531@gmail.com>
2021-03-01 00:32:58 +0800

    [Add] Add LoadMoreTableView

---
+++ LibraryCollections/LibraryCollections/component/tableview/LoadMoreTableView.swift
@@ -0,0 +1,50 @@
+import UIKit
+
+protocol ILoadMoreEvent {
+    func onLoadMore()
+}
+
+class LoadMoreTableView: UITableView, UITableViewDataSource, UITableViewDelegate {
+    
+    var loadMoreDelegage:ILoadMoreEvent?
+    var datas:[AnyObject] = []
+    var count:Int {
+        self.datas.count
+    }
+    var cellID:String!
+    var hasReachLast:Bool = false
+    
+    func addDatas(newDatas:[AnyObject]) {
+        guard newDatas.count > 0 else {
+            return
+        }
+        
+        self.hasReachLast = false
+        
+        self.datas.append(contentsOf: newDatas)
+        self.reloadData()
+    }
+    
+    func clear() {
+        datas = []
+        
+        self.reloadData()
+    }
+    
+    // MARK:- UITableViewDataSource
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        datas.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = dequeueReusableCell(withIdentifier: cellID, for: indexPath)
+        cell.selectionStyle = .none
+        
+        if !self.hasReachLast && (indexPath.row == self.datas.count - 1) {
+            self.hasReachLast = true
+            self.loadMoreDelegage?.onLoadMore()
+        }
+        
+        return cell
+    }
+}
