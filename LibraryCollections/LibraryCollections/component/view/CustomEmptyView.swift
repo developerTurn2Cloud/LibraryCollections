06c3a144be386ccadc10527f0cd0d45d99877b70
Randy <o1984531@gmail.com>
2021-03-10 16:53:46 +0800

    [Add] Add new empty view

---
+++ LibraryCollections/LibraryCollections/component/view/CustomEmptyView.swift
@@ -0,0 +1,44 @@
+import UIKit
+
+@IBDesignable public class CustomEmptyView: UIView, NibLoadable  {
+
+    @IBInspectable public  var emptyIcon:UIImage = #imageLiteral(resourceName: "ic_empty_2") {
+        didSet {
+            self.emptyImageView.image = self.emptyIcon
+        }
+    }
+    
+    @IBInspectable public var emptyTitle:String = "" {
+        didSet {
+            self.emptyLabel.text = self.emptyTitle
+        }
+    }
+    
+    @IBInspectable public var titleColor:UIColor = #colorLiteral(red: 0.7607843137, green: 0.7607843137, blue: 0.7607843137, alpha: 1) {
+        didSet {
+            self.emptyLabel.textColor = titleColor
+        }
+    }
+    
+    
+    @IBInspectable public var bgColor:UIColor = UIColor.white {
+        didSet {
+            self.contentView.backgroundColor = self.bgColor
+        }
+    }
+    
+    @IBOutlet public var contentView: UIView!
+    @IBOutlet public weak var emptyImageView: UIImageView!
+    @IBOutlet public weak var emptyLabel: UILabel!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        fromNib()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        fromNib()
+    }
+
+}
