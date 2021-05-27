b84b96a0e872e5142112d2a2e3338a6479843f2b
Randy <o1984531@gmail.com>
2021-05-27 15:42:56 +0800

    [iOS] Add shadow configs in UIButton extension

--- LibraryCollections/LibraryCollections/extension/UIButton+Extension.swift
+++ LibraryCollections/LibraryCollections/extension/UIButton+Extension.swift
@@ -1,6 +1,8 @@
 import Foundation
 import UIKit
 
+var btnShadowSpread: CGFloat = 0
+
 @IBDesignable extension UIButton {
     
     // MARK:- Title padding
@@ -41,6 +43,69 @@ import UIKit
         
         set {
             self.titleEdgeInsets.bottom = newValue
+        }
+    }
+    
+    // MARK:- Shadow
+    @IBInspectable public var shadowColor: UIColor? {
+        get {
+            if let color = layer.shadowColor {
+                return UIColor(cgColor: color)
+            }
+            return nil
+        }
+        set {
+            if let color = newValue {
+                layer.shadowColor = color.cgColor
+            } else {
+                layer.shadowColor = nil
+            }
+        }
+    }
+    
+    @IBInspectable public var shadowOpacity: Float {
+        get {
+            return layer.shadowOpacity
+        }
+        set {
+            layer.shadowOpacity = newValue
+        }
+    }
+    
+    @IBInspectable public var shadowOffset: CGPoint {
+        get {
+            return CGPoint(x: layer.shadowOffset.width, y:layer.shadowOffset.height)
+        }
+        set {
+            layer.shadowOffset = CGSize(width: newValue.x, height: newValue.y)
+        }
+        
+    }
+    
+    @IBInspectable public var shadowBlur: CGFloat {
+        get {
+            return layer.shadowRadius
+        }
+        set {
+            layer.shadowRadius = newValue / 2.0
+        }
+    }
+    
+    @IBInspectable public var shadowSpread: CGFloat {
+        set {
+            btnShadowSpread = newValue
+            
+            if btnShadowSpread == 0 {
+                layer.shadowPath = nil
+            } else {
+                let dx = -btnShadowSpread
+                let rect = bounds.insetBy(dx: dx, dy: dx)
+                layer.shadowPath = UIBezierPath(rect: rect).cgPath
+            }
+        }
+        
+        get {
+            btnShadowSpread
         }
     }
 }
