e34642fd2383c54b0c6325b46060e4c12f0fe882
Randy <o1984531@gmail.com>
2021-06-30 22:55:05 +0800

    Fix LoadMore function error

--- LibraryCollections/LibraryCollections/component/tableview/LoadMoreTableView.swift
+++ LibraryCollections/LibraryCollections/component/tableview/LoadMoreTableView.swift
@@ -12,14 +12,14 @@ open class LoadMoreTableView: UITableView, UITableViewDataSource, UITableViewDel
         self.datas.count
     }
     public var cellID:String!
-    public var hasReachLast:Bool = false
+    public var isLoading:Bool = false
     
     public func addDatas(newDatas:[AnyObject]) {
         guard newDatas.count > 0 else {
             return
         }
         
-        self.hasReachLast = false
+        self.isLoading = false
         
         self.datas.append(contentsOf: newDatas)
         self.reloadData()
@@ -31,6 +31,18 @@ open class LoadMoreTableView: UITableView, UITableViewDataSource, UITableViewDel
         self.reloadData()
     }
     
+    // MARK:- scrollViewDidScroll
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        self.hasReachLast = self.indexPathsForVisibleRows?.contains(where: { indexPath in
+            indexPath.row == self.datas.count - 1
+        }) ?? false
+        
+        if !self.isLoading {
+            self.isLoading = true
+            self.loadMoreDelegage?.onLoadMore()
+        }
+    }
+    
     // MARK:- UITableViewDataSource
    open func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         datas.count
@@ -39,11 +51,6 @@ open class LoadMoreTableView: UITableView, UITableViewDataSource, UITableViewDel
     open func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = dequeueReusableCell(withIdentifier: cellID, for: indexPath)
         cell.selectionStyle = .none
-        
-        if !self.hasReachLast && (indexPath.row == self.datas.count - 1) {
-            self.hasReachLast = true
-            self.loadMoreDelegage?.onLoadMore()
-        }
         
         return cell
     }
